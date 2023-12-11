89bfa26cae6d64f00350b048e5264e9ad99b716c
nkc53 <nkc53@github.com>
2023-12-11 12:57:41 -0700

    praacticed click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { click } from "@testing-library/user-event/dist/click";
+
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello, this is the event for clicking the ', e);
+    }
+
+    const handleCLickAgain = (name, e) => {
+        console.log('Hello, ' + name, e.target);
+    }
+
+
     return (  
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleCLickAgain('Mario', e)}>Click Me Again!</button>
         </div>
     );
 }
