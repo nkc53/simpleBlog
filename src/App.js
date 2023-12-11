d0f8b02798d199f15466776d1a63446c471556a0
nkc53 <nkc53@github.com>
2023-12-11 14:00:51 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const title = 'Welcome to my wordle dupe';
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 };
-  const link = 'https://www.linkedin.com/in/nathanchancs/';
   
   return (
     <div className="App">
