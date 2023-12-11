11db7de283b390fd551d2178bb086d602a056693
nkc53 <nkc53@github.com>
2023-12-11 12:25:01 -0700

    Practced using multiple compnonents such as a home page and navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to my wordle dupe';
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 };
-  const link = 'https://www.linkedin.com/in/nathanchancs/';
-  
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App component</h1>
+        <Home />
       </div>
     </div>
   );
