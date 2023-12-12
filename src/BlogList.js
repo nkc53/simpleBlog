a870782c40f4f60df4d7a67cdfa8e928515b34f8
nkc53 <nkc53@github.com>
2023-12-12 11:33:54 -0700

    Practicing fetching data using useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,12 @@
-const BlogList = ({ blogs, title, handleDelete}) => {
+const BlogList = ({ blogs, title}) => {
     return (  
         <div className="blog-list">
             <h2>{ title }</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author}</p>
-                    <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
+                    <h3>Written by { blog.author}</h3>
+                    <p>{ blog.body }</p>
                 </div>
             ))}
         </div>
