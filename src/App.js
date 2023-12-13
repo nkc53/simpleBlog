d465470c24784d04f20b817f72a36d6b203745d8
nkc53 <nkc53@github.com>
2023-12-13 12:00:33 -0700

    Made a 404 page using a catchall route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import BlogDetails from './BlogDetails';
 import Create from './Create';
 import Home from './Home';
 import Navbar from './Navbar';
+import NotFound from './NotFound';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
